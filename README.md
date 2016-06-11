3289f4ee916e4fd0e83ef2683317b60f162d67d2
palanik <palani.kumanan@gmail.com>
2016-06-11 02:11:19 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # root-module
-Find root module with folder and package.json of a Node.js application
+Find root module with folder and package.json.
+
+## Installation
+
+Install via npm
+
+```sh
+$ npm install root-module --save
+```
+
+## Usage
+
+```
+var rootModule = require('root-module'); 
+var m = rootModule();
+
+console.log(m.module);
+console.log(m.folder);
+console.log(m.packageFile);
+```
+
+## License
+
+  [MIT](LICENSE)
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import path from 'path';
+import fs from 'fs';
+
+function findRootModule(mod) {
+  const m = mod || module;
+  return (m.parent ? findRootModule(m.parent) : m);
+}
+
+function findFile(curDir, filename) {
+  const fullpath = path.resolve(curDir, filename);
+  try {
+    if (fs.statSync(fullpath).isFile()) {
+      return fullpath;
+    }
+  }
+  catch (e) {
+    // not a file
+  }
+
+  const par = path.resolve(curDir, '..');
+  if (par === curDir) {
+    return null;
+  }
+
+  return findFile(par, filename);
+}
+
+function rootModule(mod) {
+  const m = findRootModule(mod);
+  const folder = path.dirname(m.filename);
+  return {
+    module: m,
+    folder,
+    packageFile: findFile(folder, 'package.json')
+  };
+}
+
+module.exports = rootModule;
